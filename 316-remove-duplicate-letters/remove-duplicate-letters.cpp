bd76de5bc0734d9abb999cb7be1a6abb7b26a077
Merrill Dmonte <merrilldmonte@gmail.com>
2026-07-19 12:13:53 +0530

    Time: 1 ms (31.67%) | Memory: 9.3 MB (68.83%) - LeetSync

---
+++ 316-remove-duplicate-letters/remove-duplicate-letters.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        string ans;
+        vector<int> indexes(26);
+        vector<bool> visited(26, false);
+
+        for(int i = 0; i < s.length(); i++){
+            indexes[s[i] - 'a'] = i;
+        }
+
+        for(int i = 0; i < s.length(); i++){
+            int idx = s[i] - 'a';
+
+            if(visited[idx] == true){
+                continue;
+            }
+
+            while(ans.length() > 0 && ans.back() > s[i] && indexes[ans.back() - 'a'] > i){
+                visited[ans.back() - 'a'] = false;
+                ans.pop_back();
+            }
+
+            ans.push_back(s[i]);
+            visited[idx] = true;
+        }
+
+        return ans;
+        
+    }
+};
