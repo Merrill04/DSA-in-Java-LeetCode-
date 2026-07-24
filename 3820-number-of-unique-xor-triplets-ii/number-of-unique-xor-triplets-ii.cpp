6fdb3eee2a775939e45628c9f45e04b1bf54702c
Merrill Dmonte <merrilldmonte@gmail.com>
2026-07-24 12:35:05 +0530

    Time: 667 ms (64.04%) | Memory: 35.1 MB (83.15%) - LeetSync

--- 3820-number-of-unique-xor-triplets-ii/number-of-unique-xor-triplets-ii.cpp
+++ 3820-number-of-unique-xor-triplets-ii/number-of-unique-xor-triplets-ii.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int uniqueXorTriplets(vector<int>& nums) {
+/*      
         unordered_set<int> ans;
         unordered_set<int> v;
         
@@ -17,5 +18,44 @@ public:
         }
 
         return ans.size();
+*/
+        int max = -1;
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] > max){
+                max = nums[i];
+            }
+        }
+
+        int vsize = 1;
+        while(vsize <= max){
+            vsize *= 2;
+        }
+
+        vector<bool> one(vsize, false);
+        vector<bool> two(vsize, false);
+
+        for(int i = 0; i < nums.size(); i++){
+            for(int j = i; j < nums.size(); j++){
+                one[nums[i] ^ nums[j]] = true;
+            }
+        }
+
+        for(int i = 0; i < one.size(); i++){
+            if(one[i] == true){
+                for(int j = 0; j < nums.size(); j++){
+                    two[i ^ nums[j]] = true;
+                }
+            }
+        }
+
+        int ans = 0;
+
+        for(int i = 0; i < two.size(); i++){
+            if(two[i] == true){
+                ans++;
+            }
+        }
+
+        return ans;
     }
 };
