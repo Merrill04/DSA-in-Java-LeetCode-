5dfbfb814cdd4c4f61538b80abd8fcb8be59f00e
Merrill Dmonte <merrilldmonte@gmail.com>
2026-07-25 10:57:40 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (17.86%) - LeetSync

--- 3859-maximum-product-of-two-digits/maximum-product-of-two-digits.cpp
+++ 3859-maximum-product-of-two-digits/maximum-product-of-two-digits.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
     int maxProduct(int n) {
-        int max = -1;
-        vector<int> v;
+        int max = 1;
+        multiset<int, greater<int>> v;
 
         while(n > 0){
-            v.push_back(n % 10);
+            v.insert(n % 10);
             n /= 10;
         }
 
-        for(int i = 0; i < v.size()-1; i++){
-            for(int j = i+1; j < v.size(); j++){
-                if(v[i] * v[j] > max){
-                    max = v[i] * v[j];
-                }
-            }
-        }
+        auto it = v.begin();
+        max = max * (*it);
+        ++it;
+        max = max * (*it);
 
         return max;
     }
