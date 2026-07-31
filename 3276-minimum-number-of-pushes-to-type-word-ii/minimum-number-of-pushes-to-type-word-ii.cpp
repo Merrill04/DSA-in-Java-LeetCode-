8c2394176bb2fc1fbfc7dbc09a8476fadf9f7173
Merrill Dmonte <merrilldmonte@gmail.com>
2026-07-31 11:55:38 +0530

    Time: 16 ms (33.38%) | Memory: 25.3 MB (94.44%) - LeetSync

--- 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int minimumPushes(string word) {
+/*
+//Brute force:-
         vector<int> countarray;
         int ans = 0;
 
@@ -29,6 +31,35 @@ public:
                 ans += (countarray[i] * 3);
             }else if(i > 23 && i <= 25){
                 ans += (countarray[i] * 4);
+            }
+        }
+
+        return ans;
+*/
+        vector<int> freq(26, 0);
+        int ans = 0;
+
+        for(int i = 0; i < word.length(); i++){
+            freq[word[i] - 'a'] += 1;
+        }
+
+        sort(freq.begin(), freq.end(), greater<int>());
+
+        int count = 0; 
+
+        for(int i = 0; i < freq.size(); i++){
+            if(count <= 7 && freq[i] != 0){
+                ans += (freq[i] * 1);
+                count++;
+            }else if(count > 7 && count <= 15 && freq[i] != 0){
+                ans += (freq[i] * 2);
+                count++;
+            }else if(count > 15 && count <= 23 && freq[i] != 0){
+                ans += (freq[i] * 3);
+                count++;
+            }else if(count > 23 && count <= 25 && freq[i] != 0){
+                ans += (freq[i] * 4);
+                count++;
             }
         }
 
