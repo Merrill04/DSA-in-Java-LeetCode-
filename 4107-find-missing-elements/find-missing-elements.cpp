4fa5145d0563e30e92684881c3f26fbc3890b153
Merrill Dmonte <merrilldmonte@gmail.com>
2026-08-04 12:18:35 +0530

    Time: 1 ms (60.57%) | Memory: 32.3 MB (75.54%) - LeetSync

--- 4107-find-missing-elements/find-missing-elements.cpp
+++ 4107-find-missing-elements/find-missing-elements.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     vector<int> findMissingElements(vector<int>& nums) {
+/*
+//Approach 1:
         vector<int> ans;
         int min = 101;
         int max = 0;
@@ -21,6 +23,22 @@ public:
         for(int i = 1; i < max - min; i++){
             if(m.find(min + i) == m.end()){
                 ans.push_back(min + i);
+            }
+        }
+
+        return ans;
+*/
+//Approach 2 with constant space:
+        sort(nums.begin(), nums.end());
+        vector<int> ans;
+
+        for(int i = 1; i < nums.size(); i++){
+            if(nums[i] - nums[i-1] > 1){
+                int j = 1;
+                while(j < nums[i] - nums[i-1]){
+                    ans.push_back(nums[i-1] + j);
+                    j++;
+                }
             }
         }
 
