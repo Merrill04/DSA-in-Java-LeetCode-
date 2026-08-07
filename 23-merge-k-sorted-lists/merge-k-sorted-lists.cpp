a5d1ab2da080208563c6e10f0b0393bfaa2bc59f
Merrill Dmonte <merrilldmonte@gmail.com>
2026-08-07 13:02:08 +0530

    Time: 8 ms (20.60%) | Memory: 24.2 MB (5.00%) - LeetSync

---
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -0,0 +1,120 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* merge(ListNode* left, ListNode* right) {
+        if (left == NULL) {
+            return right;
+        }
+
+        if (right == NULL) {
+            return left;
+        }
+
+        ListNode* ans = new ListNode(-1);
+        ListNode* temp = ans;
+
+        while (left != NULL && right != NULL) {
+            if (left->val <= right->val) {
+                temp->next = left;
+                left = left->next;
+                temp = temp->next;
+            } else {
+                temp->next = right;
+                right = right->next;
+                temp = temp->next;
+            }
+        }
+
+        while (left != NULL) {
+            temp->next = left;
+            left = left->next;
+            temp = temp->next;
+        }
+
+        while (right != NULL) {
+            temp->next = right;
+            right = right->next;
+            temp = temp->next;
+        }
+
+        return ans->next;
+    }
+
+    ListNode* findmid(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+
+        while (fast != NULL && fast->next != NULL) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        return slow;
+    }
+
+    ListNode* mergesort(ListNode* head) {
+        if (head == NULL || head->next == NULL) {
+            return head;
+        }
+
+        ListNode* mid = findmid(head);
+
+        ListNode* left = head;
+        ListNode* right = mid->next;
+        mid->next = NULL;
+
+        left = mergesort(left);
+        right = mergesort(right);
+
+        ListNode* res = merge(left, right);
+
+        return res;
+    }
+
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if (lists.empty()) {
+            return NULL;
+        }
+
+        ListNode* head = lists[0];
+
+        int i = 1;
+        while (head == NULL && i < lists.size()) {
+            head = lists[i];
+            i++;
+        }
+
+        if (head == NULL) {
+            return NULL;
+        }
+
+        ListNode* temp = head;
+
+        for (; i < lists.size(); i++) {
+            if (lists[i] == NULL) {
+                continue;
+            }
+
+            while (temp->next != NULL) {
+                temp = temp->next;
+            }
+
+            temp->next = lists[i];
+
+            while (temp->next != NULL) {
+                temp = temp->next;
+            }
+        }
+
+        return mergesort(head);
+    }
+};
