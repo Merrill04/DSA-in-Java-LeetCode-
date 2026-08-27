6abbaf9e0e75da2315e8a2946ad342dac2a4ebf9
Merrill Dmonte <merrilldmonte@gmail.com>
2026-08-27 16:31:06 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (45.47%) - LeetSync

---
+++ 258-add-digits/add-digits.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        if(num < 10){
+            return num;
+        }
+
+        if(num % 9 == 0){
+            return 9;
+        }
+
+        int t = num / 9;
+        int a = t * 9;
+
+        return num - a;
+    }
+};
