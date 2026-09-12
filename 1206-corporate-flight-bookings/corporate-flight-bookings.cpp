57c1f59aa9c4fb9aa4a6804d811775d6a2cec734
Merrill Dmonte <merrilldmonte@gmail.com>
2026-09-12 15:47:25 +0530

    Time: 1 ms (65.74%) | Memory: 72.9 MB (39.55%) - LeetSync

--- 1206-corporate-flight-bookings/corporate-flight-bookings.cpp
+++ 1206-corporate-flight-bookings/corporate-flight-bookings.cpp
@@ -2,11 +2,18 @@ class Solution {
 public:
     vector<int> corpFlightBookings(vector<vector<int>>& bookings, int n) {
         vector<int> v(n, 0);
+        vector<int> diff(n+1, 0);
 
         for(int i = 0; i < bookings.size(); i++){
-            for(int j = bookings[i][0] - 1; j < bookings[i][1]; j++){
-                v[j] += bookings[i][2];
-            }
+            diff[bookings[i][0] - 1] += bookings[i][2];
+            diff[bookings[i][1]] -= bookings[i][2];
+        }
+
+        int sum = 0;
+
+        for(int i = 0; i < diff.size() - 1; i++){
+            sum += diff[i];
+            v[i] = sum;
         }
 
         return v;
