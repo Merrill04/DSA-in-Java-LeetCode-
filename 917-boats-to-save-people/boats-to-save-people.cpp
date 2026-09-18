bce3d968dd2ab486988db68df2cdc78256a7f1a1
Merrill Dmonte <merrilldmonte@gmail.com>
2026-09-18 16:28:50 +0530

    Time: 13 ms (80.60%) | Memory: 45.8 MB (38.02%) - LeetSync

---
+++ 917-boats-to-save-people/boats-to-save-people.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        int res = 0;
+        sort(people.begin(), people.end());
+
+        int left = 0;
+        int right = people.size() - 1;
+
+        while(left <= right){
+            if(people[left] + people[right] > limit){
+                right--;
+            }else{
+                left++;
+                right--;
+            }
+
+            res++;
+        }
+
+        return res;
+    }
+};
